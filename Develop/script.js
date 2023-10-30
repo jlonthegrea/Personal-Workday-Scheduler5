86bebcad9dd78454917ee541f5d1d7b1fb16692c
jlonthegrea <allen_jaylon@yahoo.com>
2023-10-30 12:48:29 -0400

    Updates

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,61 @@
+$(document).ready(function() {
+  $('.saveBTN').on('click', function () {
+    var value = $(this).siblings('.decsription').val();
+    var time = $(this).parent().attr('id');
+    
+
+
+    localStorage.setItem(time, value);
+    $('.notification').addClass('show');
+
+    setTimeout(function () {
+      $('.notification').removeClass('show');
+    }, 6000);
+  })
+});
+
+function hourUpdater() {
+
+  var currentHour = dayjs().hour();
+
+  $('.time-block').each(function  (){
+    var blockHour = parseInt($(this).attr('id').split('-')[1]);
+
+    if (currentHour > blockHour) {
+      $(this).addClass('past');
+    } else if (currentHour === blockHour) {
+      $(this).removeClass('past');
+      $(this).addClass('present');
+    } else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
+
+  });
+}
+$('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
+hourUpdater();
+setInterval(hourUpdater, 15000);
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,15 +63,21 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+
+
   // TODO: Add code to display the current date in the header of the page.
-});
+
